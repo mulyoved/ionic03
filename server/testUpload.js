4ff273e42e2ee761a44560bf02d87257e9f6f5dc
mulyoved <mulyoved@gmail.com>
2014-05-02 18:55:59 +0300

    Add server files

---
+++ server/testUpload.js
@@ -0,0 +1,32 @@
+var imageUpload = require('./imageUpload');
+var parseString = require('xml2js').parseString;
+
+imageUpload.upload('D:/js/PictureFeed/server/uploads/8140-1yodndb.jpg', { 
+        albumId:'5965097735673433505', 
+        userId: 'mulyoved',
+        token: 'ya29.1.AADtN_UPuNv2J7l6hsVcdkxrWTxRR6vjVYL6KAFsa2z0d_riWNqVgZhtxA_o-9g' 
+    }, 
+    function(error, response, body) {
+        console.log('error', error);
+        //console.log('response', response);
+        console.log('body', body);
+
+        if (body && (response.statusCode === 200 || response.statusCode === 201 || response.statusCode === 202)) {
+            parseString(body, function (err, result) {
+                console.dir(result);
+
+                if (!err) {
+                    console.dir(result.entry['media:group'][0]['media:content'][0].$.url);
+                }
+                else {
+                    console.error('Error', err);
+                }
+            });
+        }
+        else {
+            console.error('Error', response.statusCode, body);
+        }
+
+    });
+
+
